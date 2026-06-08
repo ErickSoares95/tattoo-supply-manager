719dc7167b4918ff46a6929b9bd545243b5eaa51
Erick Soares <ericklira99@gmail.com>
2026-06-08 18:27:09 -0300

    #011 - feat: more tests

--- src/test/java/com/ericksoares/tattoo/product/presentation/ProductControllerIT.java
+++ src/test/java/com/ericksoares/tattoo/product/presentation/ProductControllerIT.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -16,13 +19,125 @@ class ProductControllerIT {
     private MockMvc mockMvc;
 
     @Test
-    void shouldReturnUnauthorized() throws Exception {
+    void shouldReturnUnauthorizedWhenNoToken() throws Exception {
 
         mockMvc.perform(
                         get("/products")
                 )
                 .andExpect(
                         status().isUnauthorized()
+                );
+    }
+
+    @Test
+    @WithMockUser(
+            username = "admin",
+            roles = {"ADMIN"}
+    )
+    void shouldAllowAdminFindProducts() throws Exception {
+
+        mockMvc.perform(
+                        get("/products")
+                )
+                .andExpect(
+                        status().isOk()
+                );
+    }
+
+    @Test
+    @WithMockUser(
+            username = "employee",
+            roles = {"EMPLOYEE"}
+    )
+    void shouldAllowEmployeeFindProducts() throws Exception {
+
+        mockMvc.perform(
+                        get("/products")
+                )
+                .andExpect(
+                        status().isOk()
+                );
+    }
+
+    @Test
+    @WithMockUser(
+            username = "employee",
+            roles = {"EMPLOYEE"}
+    )
+    void shouldDenyEmployeeCreateProduct() throws Exception {
+
+        String request = """
+            {
+              "name":"Ink Black",
+              "description":"Black tattoo ink",
+              "price":99.90,
+              "stock":10
+            }
+            """;
+
+        mockMvc.perform(
+                        post("/products")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(request)
+                )
+                .andExpect(
+                        status().isForbidden()
+                );
+    }
+
+    @Test
+    @WithMockUser(
+            username = "admin",
+            roles = {"ADMIN"}
+    )
+    void shouldAllowAdminCreateProduct() throws Exception {
+
+        String request = """
+            {
+              "name":"Ink Black",
+              "description":"Black tattoo ink",
+              "price":99.90,
+              "stock":10
+            }
+            """;
+
+        mockMvc.perform(
+                        post("/products")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(request)
+                )
+                .andExpect(
+                        status().isCreated()
+                );
+    }
+
+    @Test
+    @WithMockUser(
+            username = "employee",
+            roles = {"EMPLOYEE"}
+    )
+    void shouldDenyEmployeeDeleteProduct() throws Exception {
+
+        mockMvc.perform(
+                        delete("/products/1")
+                )
+                .andExpect(
+                        status().isForbidden()
+                );
+    }
+
+    @Test
+    @WithMockUser(
+            username = "admin",
+            roles = {"ADMIN"}
+    )
+    void shouldAllowAdminDeleteProduct() throws Exception {
+
+        mockMvc.perform(
+                        delete("/products/1")
+                )
+                .andExpect(
+                        status().isNoContent()
                 );
     }
 }
